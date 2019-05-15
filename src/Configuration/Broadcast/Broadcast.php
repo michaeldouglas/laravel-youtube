99b7e8401d3e87488d7e2bf1df54ea8ba012b7dd
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2019-05-15 19:13:23 -0300

    Alpha Version

--- src/Configuration/Broadcast/Broadcast.php
+++ src/Configuration/Broadcast/Broadcast.php
@@ -1,15 +1,154 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michael
- * Date: 15/05/19
- * Time: 14:19
- */
 
 namespace Laravel\Youtube\Configuration\Broadcast;
 
+use Google_Service_YouTube_LiveBroadcast;
+use Google_Service_YouTube_LiveBroadcastContentDetails;
+use Google_Service_YouTube_LiveBroadcastSnippet;
+use Google_Service_YouTube_LiveBroadcastStatus;
+use Google_Service_YouTube_CdnSettings;
+use Google_Service_YouTube_LiveStream;
+use Google_Service_YouTube_LiveStreamSnippet;
+use Carbon\Carbon;
 
 class Broadcast
 {
+    private $liveBroadcast;
+    private $liveBroadcastContentDetails;
+    private $liveBroadcastSnippet;
+    private $liveBroadcastStatus;
+    private $broadcastsResponse;
+    private $youtubeEventId;
+    private $intialDate;
+    private $privacy;
+    private $titleEvent;
+    private $endDate;
+    private $response;
+    private $youtube;
+    private $video;
 
+    public function __construct($app)
+    {
+        $this->app                         = $app;
+        $this->liveBroadcast               = new Google_Service_YouTube_LiveBroadcast();
+        $this->liveBroadcastContentDetails = new Google_Service_YouTube_LiveBroadcastContentDetails();
+        $this->liveBroadcastSnippet        = new Google_Service_YouTube_LiveBroadcastSnippet();
+        $this->liveBroadcastStatus         = new Google_Service_YouTube_LiveBroadcastStatus();
+    }
+
+    private function adjustDate(String $dateParam)
+    {
+        $dataFormated = Carbon::createFromFormat('Y-m-d H:i:s', $dateParam, $this->app->config->get('youtube.timezone'));
+        $dataFormated = ($dataFormated < Carbon::now($this->app->config->get('youtube.timezone'))) ? Carbon::now($this->app->config->get('youtube.timezone')) : $dataFormated;
+        return $dataFormated->toIso8601String();
+    }
+
+    private function setLiveBroadcast()
+    {
+        $this->liveBroadcastContentDetails->setEnableClosedCaptions(true);
+        $this->liveBroadcastContentDetails->setEnableContentEncryption(true);
+        $this->liveBroadcastContentDetails->setEnableDvr(true);
+        $this->liveBroadcastContentDetails->setEnableEmbed(true);
+        $this->liveBroadcastContentDetails->setRecordFromStart(true);
+        $this->liveBroadcastContentDetails->setStartWithSlate(true);
+        $this->liveBroadcastContentDetails->setEnableAutoStart(true);
+        $this->liveBroadcast->setContentDetails($this->liveBroadcastContentDetails);
+
+        return $this;
+    }
+
+    private function setSnippet($intialDate, $endDate, $titleEvent)
+    {
+        if(!is_null($endDate) && $endDate != "" && !empty($endDate)){
+            $this->liveBroadcastSnippet->setScheduledEndTime($this->adjustDate($endDate));
+        }
+        $this->liveBroadcastSnippet->setScheduledStartTime($this->adjustDate($intialDate));
+        $this->liveBroadcastSnippet->setTitle($titleEvent);
+        $this->liveBroadcast->setSnippet($this->liveBroadcastSnippet);
+
+        return $this;
+    }
+
+    /**
+     * Add 'status' object to the $liveBroadcast object.
+     * @param $privacy
+     * @return $this
+     */
+    private function setLiveStatus($privacy)
+    {
+        $this->liveBroadcastStatus->setPrivacyStatus($privacy);
+        $this->liveBroadcast->setStatus($this->liveBroadcastStatus);
+        $this->liveBroadcast->setKind('youtube#liveBroadcast');
+
+        return $this;
+    }
+
+    private function listVideo($youtube_event_id)
+    {
+        $listResponse = $this->youtube->videos->listVideos("snippet", array('id' => $youtube_event_id));
+        $this->video = $listResponse[0];
+    }
+
+    private function creteEventBroadcast()
+    {
+        $this->broadcastsResponse = $this->youtube->liveBroadcasts->insert('snippet,contentDetails,status', $this->liveBroadcast);
+
+        $this->response['broadcast_response'] = $this->broadcastsResponse;
+
+        $this->youtubeEventId = $this->broadcastsResponse['id'];
+
+        return $this;
+    }
+
+    private function setDefaultConfigurations($intialDate, $endDate, $titleEvent, $privacy, $objectYouTube)
+    {
+        $this->youtube    = $objectYouTube;
+        $this->intialDate = $intialDate;
+        $this->endDate    = $endDate;
+        $this->titleEvent = $titleEvent;
+        $this->privacy    = $privacy;
+    }
+
+    public function createEvent($intialDate, $endDate, $titleEvent, $privacy, $objectYouTube)
+    {
+        $this->setDefaultConfigurations($intialDate, $endDate, $titleEvent, $privacy, $objectYouTube);
+
+        $this->setLiveBroadcast()->setSnippet($intialDate, $endDate, $titleEvent)->setLiveStatus($privacy)->creteEventBroadcast();
+
+        $this->listVideo($this->youtubeEventId);
+
+        $videoSnippet = $this->video['snippet'];
+        $videoSnippet['tags'] = ['video'];
+        $videoSnippet['defaultAudioLanguage'] = "pt-BR";
+        $videoSnippet['defaultLanguage'] = "pt-BR";
+
+        $this->video['snippet'] = $videoSnippet;
+
+        $updateResponse = $this->youtube->videos->update("snippet", $this->video);
+        $this->response['video_response'] = $updateResponse;
+
+        //object of livestream resource title
+        $googleYoutubeLiveStreamSnippet = new Google_Service_YouTube_LiveStreamSnippet;
+        $googleYoutubeLiveStreamSnippet->setTitle($titleEvent);
+
+        $googleYoutubeCdnSettings = new Google_Service_YouTube_CdnSettings;
+        $googleYoutubeCdnSettings->setFormat("1080p");
+        $googleYoutubeCdnSettings->setIngestionType('rtmp');
+
+        // API request inserts liveStream resource.
+        $googleYoutubeLiveStream = new Google_Service_YouTube_LiveStream;
+        $googleYoutubeLiveStream->setSnippet($googleYoutubeLiveStreamSnippet);
+        $googleYoutubeLiveStream->setCdn($googleYoutubeCdnSettings);
+        $googleYoutubeLiveStream->setKind('youtube#liveStream');
+
+        //execute the insert request [return an object that contains information about new stream]
+        $streamsResponse = $this->youtube->liveStreams->insert('snippet,cdn', $googleYoutubeLiveStream, array());
+        $this->response['stream_response'] = $streamsResponse;
+
+        //Bind the broadcast to the live stream
+        $bindBroadcastResponse = $this->youtube->liveBroadcasts->bind($this->broadcastsResponse['id'], 'id,contentDetails', ['streamId' => $streamsResponse['id'],]);
+        $this->response['bind_broadcast_response'] = $bindBroadcastResponse;
+
+        return $this->response;
+    }
 }
--- src/Configuration/Setup.php
+++ src/Configuration/Setup.php
@@ -3,19 +3,11 @@
 namespace Laravel\Youtube\Configuration;
 
 use Google_Client;
-use Google_Service_YouTube_LiveBroadcast;
-use Google_Service_YouTube_LiveBroadcastContentDetails;
-use Google_Service_YouTube_LiveBroadcastSnippet;
-use Google_Service_YouTube_LiveBroadcastStatus;
-use Google_Service_YouTube_CdnSettings;
-use Google_Service_YouTube_LiveStream;
-use Google_Service_YouTube_LiveStreamSnippet;
-use Carbon\Carbon;
+use Laravel\Youtube\Configuration\Broadcast\Broadcast;
 use Exception;
 
 class Setup
 {
-
     private $idClient;
     private $secretClient;
     private $scopes;
@@ -24,6 +16,7 @@ class Setup
     private $app;
     private $config;
     private $client;
+    private $broadcast;
 
     /**
      * Setup constructor.
@@ -34,6 +27,7 @@ class Setup
     public function __construct($app, Google_Client $client)
     {
         $this->app = $app;
+        $this->broadcast = new Broadcast($app);
         $this->config = $this->app->config;
         $this->getConfigurations();
 
@@ -74,87 +68,6 @@ class Setup
 
     public function getClientBroadcasting(String $intialDate, String $endDate, String $titleEvent, String $privacy, $objectYouTube)
     {
-        return $this->setClientBroadcasting($intialDate, $endDate, $titleEvent, $privacy, $objectYouTube);
-    }
-
-    private function adjustDate(String $dateParam)
-    {
-        $dataFormated = Carbon::createFromFormat('Y-m-d H:i:s', $dateParam, $this->app->config->get('youtube.timezone'));
-        $dataFormated = ($dataFormated < Carbon::now($this->app->config->get('youtube.timezone'))) ? Carbon::now($this->app->config->get('youtube.timezone')) : $dataFormated;
-        return $dataFormated->toIso8601String();
-    }
-
-    private function setClientBroadcasting($intialDate, $endDate, $titleEvent, $privacy, $objectYouTube)
-    {
-        // Define the $liveBroadcast object, which will be uploaded as the request body.
-        $liveBroadcast = new Google_Service_YouTube_LiveBroadcast();
-
-        // Add 'contentDetails' object to the $liveBroadcast object.
-        $liveBroadcastContentDetails = new Google_Service_YouTube_LiveBroadcastContentDetails();
-        $liveBroadcastContentDetails->setEnableClosedCaptions(true);
-        $liveBroadcastContentDetails->setEnableContentEncryption(true);
-        $liveBroadcastContentDetails->setEnableDvr(true);
-        $liveBroadcastContentDetails->setEnableEmbed(true);
-        $liveBroadcastContentDetails->setRecordFromStart(true);
-        $liveBroadcastContentDetails->setStartWithSlate(true);
-        $liveBroadcastContentDetails->setEnableAutoStart(true);
-        $liveBroadcast->setContentDetails($liveBroadcastContentDetails);
-
-        // Add 'snippet' object to the $liveBroadcast object.
-        $liveBroadcastSnippet = new Google_Service_YouTube_LiveBroadcastSnippet();
-        $liveBroadcastSnippet->setScheduledEndTime($this->adjustDate($endDate));
-        $liveBroadcastSnippet->setScheduledStartTime($this->adjustDate($intialDate));
-        $liveBroadcastSnippet->setTitle($titleEvent);
-        $liveBroadcast->setSnippet($liveBroadcastSnippet);
-
-        // Add 'status' object to the $liveBroadcast object.
-        $liveBroadcastStatus = new Google_Service_YouTube_LiveBroadcastStatus();
-        $liveBroadcastStatus->setPrivacyStatus($privacy);
-        $liveBroadcast->setStatus($liveBroadcastStatus);
-        $liveBroadcast->setKind('youtube#liveBroadcast');
-
-        // Create Event
-        $broadcastsResponse = $objectYouTube->liveBroadcasts->insert('snippet,contentDetails,status', $liveBroadcast);
-
-        $response['broadcast_response'] = $broadcastsResponse;
-
-        $youtube_event_id = $broadcastsResponse['id'];
-
-        $listResponse = $objectYouTube->videos->listVideos("snippet", array('id' => $youtube_event_id));
-        $video = $listResponse[0];
-
-        $videoSnippet = $video['snippet'];
-        $videoSnippet['tags'] = ['video'];
-        $videoSnippet['defaultAudioLanguage'] = "pt-BR";
-        $videoSnippet['defaultLanguage'] = "pt-BR";
-
-        $video['snippet'] = $videoSnippet;
-
-        $updateResponse = $objectYouTube->videos->update("snippet", $video);
-        $response['video_response'] = $updateResponse;
-
-        //object of livestream resource title
-        $googleYoutubeLiveStreamSnippet = new Google_Service_YouTube_LiveStreamSnippet;
-        $googleYoutubeLiveStreamSnippet->setTitle($titleEvent);
-
-        $googleYoutubeCdnSettings = new Google_Service_YouTube_CdnSettings;
-        $googleYoutubeCdnSettings->setFormat("1080p");
-        $googleYoutubeCdnSettings->setIngestionType('rtmp');
-
-        // API request inserts liveStream resource.
-        $googleYoutubeLiveStream = new Google_Service_YouTube_LiveStream;
-        $googleYoutubeLiveStream->setSnippet($googleYoutubeLiveStreamSnippet);
-        $googleYoutubeLiveStream->setCdn($googleYoutubeCdnSettings);
-        $googleYoutubeLiveStream->setKind('youtube#liveStream');
-
-        //execute the insert request [return an object that contains information about new stream]
-        $streamsResponse = $objectYouTube->liveStreams->insert('snippet,cdn', $googleYoutubeLiveStream, array());
-        $response['stream_response'] = $streamsResponse;
-
-        //Bind the broadcast to the live stream
-        $bindBroadcastResponse = $objectYouTube->liveBroadcasts->bind($broadcastsResponse['id'], 'id,contentDetails', ['streamId' => $streamsResponse['id'],]);
-        $response['bind_broadcast_response'] = $bindBroadcastResponse;
-
-        return $response;
+        return $this->broadcast->createEvent($intialDate, $endDate, $titleEvent, $privacy, $objectYouTube);
     }
 }
